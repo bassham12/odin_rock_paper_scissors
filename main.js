f66d771ed49681f4b68a917d9292c7fd0e8e62cb
Samuel Bassham <42912617+bassham12@users.noreply.github.com>
2022-09-10 09:16:00 -0500

    dom variables and global variables

--- main.js
+++ main.js
@@ -1,60 +1,14 @@
-const input = document.querySelector(".player-choice");
-const inputBtn = document.querySelector(".input-btn");
-const output = document.querySelector(".output");
-const playerPointsOutput = document.querySelector(".player-points");
-const computerPointsOutput = document.querySelector(".computer-points");
+// DOM VARIABLES
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorBtn = document.getElementById("scissors-btn");
+const playerScoreCard = document.getElementById("player-score-card");
+const computerScoreCard = document.getElementById("computer-score-card");
 
-// make a array of "rock", "paper", "scissors"
+// GLOBAL VARIABLES
 const choices = ["rock", "paper", "scissors"];
-let computersSelection = getComputerChoice(choices);
-let playerSelection = getPlayerChoice();
-let computerPoints = 0;
-let playerPoints = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 
-function getPlayerChoice() {
-  let selection;
-  inputBtn.addEventListener('click', () => {
-    selection = input.value;
-    input.value = "";
-    return selection;    
-  });
-}
 
-// get a random choice between rock, paper, or scissors
-function getComputerChoice(choices) {  
-  let randomNum = Math.floor(Math.random() * (choices.length));
-  let selection = choices[randomNum];
-  console.log(selection);
-  return selection
-}
-
-//game round takes in the choice of the player and the computer
-
-function gameRound(playerSelection, computerSelection) {
-  let message = "";
-  // compare playerSelection and computerSelection
-  if (
-    playerSelection == "rock" && computerSelection == "scissors" ||
-    playerSelection == "scissors" && computerSelection == "paper" ||
-    playerSelection == "paper" && computerSelection == "rock"
-  )
-  {
-    playerPoints++;
-    message = `You Win! ${playerSelector} beats ${computerSelector}`; 
-  } else {
-    computerPoints++;
-    message = `You Lose! ${computerSelector} beats ${playerSelector}`
-  }
-  // if the player loses print "You Lose! Paper beats Rock"
-  // if the player wins print "You win! Paper beats Rock"
-  output.innerHTML = message;
-  playerPointsOutput.innerHTML = `player \n ${playerPoints}`;
-  computerPointsOutput.innerHTML = `computer \n ${computerPoints}`;
-}
-
-
-function game() {
-  gameRound(playerSelection, computerSelection);
-}
-game();

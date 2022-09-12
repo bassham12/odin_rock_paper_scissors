40f4f2ef24d0b7c0216fc6c5e430529a424bdc79
Samuel Bassham <42912617+bassham12@users.noreply.github.com>
2022-09-11 22:56:15 -0500

    announce computer's selection

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const playerInput = document.getElementById("weapon");
+const computerSelectionDiv = document.getElementById("computer-selection");
 const playerSeletionDiv = document.getElementById("player-selection"); 
 const rockBtn = document.getElementById("rock-btn");
 const paperBtn = document.getElementById("paper-btn");
@@ -11,47 +11,58 @@ let computerScore = 0;
 
 rockBtn.addEventListener('click', () => {
   let playerChoice = rockBtn.value;
+  
+  computerSelectionDiv.lastChild.textContent = "";
   gameRound(playerChoice, getComputerChoice());
+  
 });
 
 paperBtn.addEventListener('click', () => {
   let playerChoice = paperBtn.value;
+
+  computerSelectionDiv.lastChild.textContent = "";
   gameRound(playerChoice, getComputerChoice());
 });
 
 scissorsBtn.addEventListener('click', () => {
   let playerChoice = scissorsBtn.value;
+
+  computerSelectionDiv.lastChild.textContent = "";
   gameRound(playerChoice, getComputerChoice());
 });
+
+
+
+function getComputerChoice() {
+  let index = random(choices.length);
+  let choice = choices[index];
+  let span = document.createElement("span");
+  span.textContent = choice;
+  computerSelectionDiv.append(span);
+  return choice;
+}
+
+function random(number) {
+  return Math.floor(Math.random() * number);
+}
 
 function resetScore() {
   playerScore = 0;
   computerScore = 0;
 }
 
-function getComputerChoice() {
-  let index = random(choices.length);
-  let choice = choices[index];
-  return choice;
-}
-
-
-function random(number) {
-  return Math.floor(Math.random() * number);
-}
-
 function checkScore(playerScore, computerScore) {
   if (playerScore == 5) {
-    playerScore = 0;
-    computerScore = 0;
+    resetScore();
     console.log("player won");
   }
   if (computerScore == 5) {
-    playerScore = 0;
-    computerScore = 0;
+    resetScore();
     console.log("computer won");
   }
 }
+
+
 
 function gameRound(playerChoice, computerChoice) {
 
@@ -76,8 +87,11 @@ function gameRound(playerChoice, computerChoice) {
     console.log(`You lose this round. ${computerChoice} beats ${playerChoice} \n computer score: ${computerScore}`);
   }
   else console.log("You tied with the computer");
+
+  scoreboard(playerScore, computerScore);
   
   checkScore(playerScore, computerScore);
+  
 }
 
 
